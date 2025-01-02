68a852f797502cfd510d43960457a89059849d20
Sergey Boykov <sutemi67@gmail.com>
2025-01-03 01:44:47 +0300

    patched

--- app/src/main/java/apc/appcradle/algorithms_binarysearch_app/MainActivity.kt
+++ app/src/main/java/apc/appcradle/algorithms_binarysearch_app/MainActivity.kt
@@ -1,13 +1,13 @@
 package apc.appcradle.algorithms_binarysearch_app
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import apc.appcradle.algorithms_binarysearch_app.databinding.ActivityMainBinding
 import java.util.Locale
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -37,42 +37,53 @@ class MainActivity : AppCompatActivity() {
                     var i = -1
                     val array = Array(count, { i++ })
                     oneByOneSearch(array, searchNumber)
-
+                    binarySearch(array, searchNumber)
                 } catch (e: Exception) {
                     binding.itemToSearchText.text?.clear()
+                    binding.numberOfArrayText.text?.clear()
                 }
             }
         }
     }
 
     private fun oneByOneSearch(array: Array<Int>, searchItem: Int) {
-        val start = System.currentTimeMillis()
-        for (i in array.indices) {
-            Log.d("TAG", "$searchItem ищем этот элемент, сейчас индекс равен $i")
-            if (array[i] == searchItem) {
-                break
+        val time = measureTimeMillis {
+            for (i in array.indices) {
+                if (array[i] == searchItem) {
+                    break
+                }
+                summaryOfOne++
             }
-            summaryOfOne++
         }
-        val resultTime = (System.currentTimeMillis() - start).toString()
         binding.oneCountText.text =
             String.format(Locale.getDefault(), "$summaryOfOne operations")
         binding.oneCountTextMs.text =
-            String.format(Locale.getDefault(), "$resultTime ms")
+            String.format(Locale.getDefault(), "$time ms")
         summaryOfOne = 0
     }
 
-//    private fun binarySearch() {
-//        var x = -1
-//        val array2 = Array(count, { i++ })
-//        var y = 0
-//        val start2 = System.currentTimeMillis()
-//        val low = 0
-//        val high = array2.size
-//        while (low <= high) {
-//            val mid = (low + high) / 2
-//            val guess = array2[mid]
-//            if (guess == array2[count - 1])
-//        }
-//    }
+    private fun binarySearch(array: Array<Int>, searchItem: Int) {
+        val time = measureTimeMillis {
+            var low = 0
+            var high = array.size
+            var mid = 0
+            while (low <= high) {
+                mid = (low + high) / 2
+                val guess = array[mid]
+                summaryOfSecond++
+                if (guess == searchItem) {
+                    break
+                } else if (guess > searchItem) {
+                    high = mid - 1
+                } else {
+                    low = mid + 1
+                }
+            }
+        }
+        binding.algoCountText.text =
+            String.format(Locale.getDefault(), "$summaryOfSecond operations")
+        binding.algoTimeText.text =
+            String.format(Locale.getDefault(), "$time ms")
+        summaryOfSecond = 0
+    }
 }
